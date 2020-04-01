1959e35aada2eac85fddc1bb17ccc38c6fe407b0
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-01 14:59:05 +0100

    Building sets of characters

--- script.js
+++ script.js
@@ -7,6 +7,20 @@
 // The function that creates the password should concatenate the sets depending on the user input and randomly sweep through and pick a specific number of characters that builds the password.
 
 
+const charAmountUser = document.getElementById("character-amount")
+
+
+
+
+// taking the input of how many characters the pass should contain and put the input in charAmount
+function charAmountFunc(event){
+    const val = event.target.value
+    charAmount.value = val
+}
+
+charAmount.addEventListener("input", charAmountFunc);
+
+
 
 
 
@@ -29,6 +43,17 @@ const LowCaseASCII = genArray(97, 122)
 const UppCaseASCII = genArray(65, 90)
 const NumASCII = genArray(48, 57)
 const SymASCII = genArray(33, 47).concat(genArray(58, 64)).concat(genArray(91, 96)).concat(genArray(123, 126))
+
+
+function genPass(charAmount, LowCaseASCII){
+    const charCodes = LowCaseASCII
+    const passowrdChar = []
+    for (let i=0; i<charAmountUser; i++){
+        const characterCode = charCodes[Math.floor(math.random()*charCodes.length)]
+        passowrdChar.push(string.fromCharCode(characterCode))
+    }
+    return passowrdChar.join('')
+}
 
 
 
